058e39e840ec55045b6e9fa641f2a36ebed3c45e
sanc00 <jonathan.sanchez00@gmail.com>
2025-05-29 14:45:17 -0400

    created loginPage

---
+++ page-models/loginPage.ts
@@ -0,0 +1,18 @@
+import { Selector, t } from 'testcafe';
+
+class LoginPage {
+    // Selectors
+    
+    constructor() {
+        
+    }
+
+    // Actions
+    async login(): Promise<void> {
+
+    }
+
+
+}
+
+export default new LoginPage();
